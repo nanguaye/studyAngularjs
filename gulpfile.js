f939e0d6f536c9cd39a2193da2a6b384fc272cfd
yechenhao <ych@xkeshi.com>
2016-04-25 16:00:51 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// 引入 gulp
+var gulp = require('gulp');
+
+// 引入组件
+var ip = require('ip');
+var connect=require('gulp-connect');
+
+var addr=ip.address();  //ip地址
+
+gulp.task('ipServer',function () {
+    connect.server({
+        host:addr,
+        root:'./',
+        port:8090
+    })
+});
+
+
+gulp.task('default', ['ipServer'], function () {
+    console.log('启动ipServer服务');
+});
---
+++ views/yeapp/directive/directiveye.js
@@ -0,0 +1,9 @@
+/**
+ * Created by nangua on 16/4/25.
+ */
+myApp.directive('item',function () {
+   return{
+       restrict:'E',
+       templateUrl:"./yeapp/directive/itemTemplate.html"
+   } 
+});
